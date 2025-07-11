2a819998fbfb76bdeb47f7efb16aabe68907eece
jlstout1998 <jlstout1998@gmail.com>
2025-07-10 17:49:29 -0700

    Update ISeeLavaClientMod.java
    Added another import to fix translucent

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -8,7 +8,8 @@ import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 // import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.BlockRenderLayer;
+import net.minecraft.client.renderer.BlockRenderLayer; // 1st
+import net.minecraft.client.render.RenderLayers; // 2nd
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.material.Fluids;
 
