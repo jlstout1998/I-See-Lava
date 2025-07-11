8ac1a04531ca43c79605a74831dbc1629300b491
jlstout1998 <jlstout1998@gmail.com>
2025-07-11 00:26:50 -0700

    Update ISeeLavaClientMod.java

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -10,9 +10,10 @@ import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 // import net.minecraft.client.renderer.RenderType;
-import net.minecraft.resources.ResourceLocation;
+// import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Identifier; // Yarn's ResourceLocation
 // import net.minecraft.world.level.material.Fluids;
-import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluid; // Yarn's Fluids
 
 // @Environment(EnvType.CLIENT)
 public class ISeeLavaClientMod implements ClientModInitializer {
