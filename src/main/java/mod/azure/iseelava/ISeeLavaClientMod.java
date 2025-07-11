22197d3751d0a104684425cfd2e27bfeb77be14d
jlstout1998 <jlstout1998@gmail.com>
2025-07-10 18:28:38 -0700

    Update ISeeLavaClientMod.java

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 // import net.minecraft.client.renderer.RenderType;
 // import net.minecraft.client.renderer.BlockRenderLayer;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.material.Fluids;
 
