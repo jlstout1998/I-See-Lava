3a6b0f3910303fbd3e7a697574ce917e7e10cd94
jlstout1998 <jlstout1998@gmail.com>
2025-07-11 01:53:51 -0700

    Update ISeeLavaClientMod.java
    Cleaning out older sections

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -3,20 +3,16 @@ package mod.azure.iseelava;
 import net.minecraft.client.render.BlockRenderLayer;
 
 import net.fabricmc.api.ClientModInitializer;
-// import net.fabricmc.api.EnvType;
-// import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
-// import net.minecraft.client.renderer.RenderType;
 // import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Identifier; // Yarn's ResourceLocation
 // import net.minecraft.world.level.material.Fluids;
 import net.minecraft.fluid.Fluids; // Yarn's Fluids
 import net.minecraft.fluid.Fluid;
 
-// @Environment(EnvType.CLIENT)
 public class ISeeLavaClientMod implements ClientModInitializer {
 	public static final String ID = "iseelava";
 
