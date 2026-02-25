8c5defa5e133158821b653b193f19cb35828ecd8
jlstout1998 <jlstout1998@gmail.com>
2026-02-24 16:13:45 -0800

    Comment out translucent lava rendering code
    Comment out the line that makes lava translucent.

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -2,7 +2,6 @@ package mod.azure.iseelava;
 
 import net.fabricmc.api.ClientModInitializer;
 // import net.fabricmc.fabric.api.client.rendering.v1.ChunkSectionLayerMap;
-import net.fabricmc.fabric.impl.client.rendering.ChunkSectionLayerMapImpl;
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.fabricmc.fabric.api.resource.v1.pack.PackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
@@ -23,7 +22,7 @@ public class ISeeLavaClientMod implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// Make lava translucent by assigning it to the translucent render layer
-		ChunkSectionLayerMapImpl.putFluids(ChunkSectionLayer.TRANSLUCENT, Fluids.LAVA, Fluids.FLOWING_LAVA);
+		// ChunkSectionLayerMapImpl.putFluids(ChunkSectionLayer.TRANSLUCENT, Fluids.LAVA, Fluids.FLOWING_LAVA);
 
 		 // Register the built-in resource pack for translucent lava
 		Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(ID);
