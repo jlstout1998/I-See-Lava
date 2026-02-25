a767563e7fdad6fc9943493a3a2a6d30f1a85caa
jlstout1998 <jlstout1998@gmail.com>
2026-02-24 16:52:23 -0800

    Update method to use ChunkSectionLayerMap

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -22,7 +22,7 @@ public class ISeeLavaClientMod implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// Make lava translucent by assigning it to the translucent render layer
-		// ChunkSectionLayerMapImpl.putFluids(ChunkSectionLayer.TRANSLUCENT, Fluids.LAVA, Fluids.FLOWING_LAVA);
+		// ChunkSectionLayerMap.putFluids(ChunkSectionLayer.TRANSLUCENT, Fluids.LAVA, Fluids.FLOWING_LAVA);
 
 		 // Register the built-in resource pack for translucent lava
 		Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(ID);
