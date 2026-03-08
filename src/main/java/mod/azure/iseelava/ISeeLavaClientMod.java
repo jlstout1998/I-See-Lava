50c270291f50b1c5125dd324d2b2b21d6116b834
jlstout1998 <jlstout1998@gmail.com>
2026-03-08 00:19:40 -0800

    Clean up imports and remove lava render handler
    Removed commented-out imports and fluid render handler registration for lava.

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -2,14 +2,12 @@ package mod.azure.iseelava;
 
 import net.fabricmc.api.ClientModInitializer;
 // import net.fabricmc.fabric.api.client.rendering.v1.ChunkSectionLayerMap;
-import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
-import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.fabricmc.fabric.api.resource.v1.pack.PackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 // import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
-import net.minecraft.world.level.material.Fluids;
+// import net.minecraft.world.level.material.Fluids;
 import net.minecraft.resources.Identifier;
 
 import java.util.Optional;
@@ -29,8 +27,5 @@ public class ISeeLavaClientMod implements ClientModInitializer {
 		 // Register the built-in resource pack for translucent lava
 		Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(ID);
 		modContainer.ifPresent(container -> ResourceLoader.registerBuiltinPack(TRANSLUCENT_LAVA_RP, container, PackActivationType.DEFAULT_ENABLED));
-
-		// Register a FluidRenderHandler for lava using alpha textures
-		FluidRenderHandlerRegistry.INSTANCE.register(Fluids.LAVA, Fluids.FLOWING_LAVA, new SimpleFluidRenderHandler(Identifier.fromNamespaceAndPath("minecraft", "block/lava_still"), Identifier.fromNamespaceAndPath("minecraft", "block/lava_flow")));
 	}
 }
