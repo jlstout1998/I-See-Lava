78d05fe0798bbb7f05fe2166d4f847a26db3737e
jlstout1998 <jlstout1998@gmail.com>
2026-03-08 01:58:48 -0800

    Update lava rendering to use new fluid handler
    Replace deprecated rendering methods for lava fluid with new FluidRenderHandlerRegistry and SimpleFluidRenderHandler.

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -2,12 +2,16 @@ package mod.azure.iseelava;
 
 import net.fabricmc.api.ClientModInitializer;
 // import net.fabricmc.fabric.api.client.rendering.v1.ChunkSectionLayerMap;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry; // REPLACEMENT
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler; // REPLACEMENT
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.fabricmc.fabric.api.resource.v1.pack.PackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
-// import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
-// import net.minecraft.world.level.material.Fluids;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import net.minecraft.client.resources.model.ModelBakery; // REPLACEMENT
+import net.minecraft.client.resources.model.sprite.SpriteGetter; // REPLACEMENT
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.resources.Identifier;
 
 import java.util.Optional;
@@ -24,6 +28,15 @@ public class ISeeLavaClientMod implements ClientModInitializer {
 		// Make lava translucent by assigning it to the translucent render layer
 		// ChunkSectionLayerMap.putFluids(ChunkSectionLayer.TRANSLUCENT, Fluids.LAVA, Fluids.FLOWING_LAVA);
 
+		FluidRenderHandlerRegistry.INSTANCE.register(Fluids.LAVA, Fluids.FLOWING_LAVA, new SimpleFluidRenderHandler(ModelBakery.LAVA_STILL, ModelBakery.LAVA_FLOW) {
+				@Override
+				public ChunkSectionLayer reloadTextures(SpriteGetter spriteGetter) {
+					super.reloadTextures(spriteGetter);
+					return ChunkSectionLayer.TRANSLUCENT;
+				}
+			}
+		);
+		
 		 // Register the built-in resource pack for translucent lava
 		Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(ID);
 		modContainer.ifPresent(container -> ResourceLoader.registerBuiltinPack(TRANSLUCENT_LAVA_RP, container, PackActivationType.DEFAULT_ENABLED));
