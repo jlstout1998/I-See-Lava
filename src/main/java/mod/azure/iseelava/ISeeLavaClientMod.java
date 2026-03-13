050b3df029a95330e40a8e5d9751190afd34d889
jlstout1998 <jlstout1998@gmail.com>
2026-03-12 17:09:39 -0700

    Test Fallback Code
    Updated fluid rendering for translucent lava and removed commented code.

--- src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
+++ src/main/java/mod/azure/iseelava/ISeeLavaClientMod.java
@@ -1,17 +1,15 @@
 package mod.azure.iseelava;
 
 import net.fabricmc.api.ClientModInitializer;
-// import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
-// import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.fabricmc.fabric.api.resource.v1.pack.PackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
-// import net.minecraft.client.renderer.chunk.ChunkSectionLayer; // OPTIONAL
-// import net.minecraft.client.resources.model.ModelBakery; // OPTIONAL
-// import net.minecraft.client.resources.model.sprite.SpriteGetter; // OPTIONAL
+import net.minecraft.client.renderer.block.FluidModel;
 import net.minecraft.resources.Identifier;
-// import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.material.Fluids;
 
 import java.util.Optional;
 
@@ -23,21 +21,20 @@ public class ISeeLavaClientMod implements ClientModInitializer {
 	private static final Identifier TRANSLUCENT_LAVA_RP = Identifier.fromNamespaceAndPath(ID, "translucent_lava");
 
 	@Override
-	public void onInitializeClient() {
-		/*
-		// Make lava translucent by assigning it to the translucent render layer
-		FluidRenderHandlerRegistry.INSTANCE.register(Fluids.LAVA, Fluids.FLOWING_LAVA, new SimpleFluidRenderHandler(ModelBakery.LAVA_STILL, ModelBakery.LAVA_FLOW) {
-				// OPTIONAL - force translucent render layer
-				@Override
-				public ChunkSectionLayer reloadTextures(SpriteGetter spriteGetter) {
-					super.reloadTextures(spriteGetter);
-					return ChunkSectionLayer.TRANSLUCENT;
-				}
-			}
-		); */
-		
+	public void onInitializeClient() {		
 		 // Register the built-in resource pack for translucent lava
 		Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(ID);
 		modContainer.ifPresent(container -> ResourceLoader.registerBuiltinPack(TRANSLUCENT_LAVA_RP, container, PackActivationType.DEFAULT_ENABLED));
+
+		// Register lava with a translucent fluid model and fallback handler
+        FluidModel.Unbaked model = new FluidModel.Unbaked(
+                new Identifier("minecraft", "block/lava_still"),
+                new Identifier("minecraft", "block/lava_flow"),
+                null, // no overlay
+                null  // no tint
+        );
+
+        FluidRenderHandler handler = FluidRenderingRegistry.getOverride(Fluids.LAVA);
+        FluidRenderingRegistry.register(Fluids.LAVA, Fluids.FLOWING_LAVA, model, handler != null ? handler : FluidRenderHandler::renderFluid);
 	}
 }
