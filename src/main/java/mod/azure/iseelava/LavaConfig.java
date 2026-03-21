d4013f8870e9c64af3e3f2920c385f41a204fa91
jlstout1998 <jlstout1998@gmail.com>
2026-03-20 22:11:27 -0700

    Update LavaConfig.java

--- src/main/java/mod/azure/iseelava/LavaConfig.java
+++ src/main/java/mod/azure/iseelava/LavaConfig.java
@@ -2,5 +2,5 @@ package mod.azure.iseelava;
 
 public class LavaConfig {
     /** Opacity factor for lava (0.0 = fully transparent, 1.0 = fully opaque) */
-    public static final float OPACITY = 0.1f; // adjust as desired
+    public static final float OPACITY = 0.5f; // adjust as desired
 }
